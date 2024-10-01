0649261beb1c705655fb2fa5cae298c7abd2ef5c
Dominik Bubenik <bubenik1@stud.uniza.sk>
2024-10-01 17:59:34 +0200

    adding buttons to navbar

--- printcetnrum_web.client/src/app/app.module.ts
+++ printcetnrum_web.client/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
+    BrowserModule, HttpClientModule, MatToolbarModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- printcetnrum_web.client/src/app/navbar/navbar.component.ts
+++ printcetnrum_web.client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  currentUrl: string | undefined;
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
+  ngOnInit(): void {
+    this.currentUrl = this.router.url;
+
+    this.router.events.subscribe(() => {
+      this.currentUrl = this.router.url;
+    });
+  }
 }
