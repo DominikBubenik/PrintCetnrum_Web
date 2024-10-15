02c19c6966c52ea63e91bceedd71834f6dbfc4f2
Dominik Bubenik <bubenik1@stud.uniza.sk>
2024-10-15 16:34:57 +0200

    navbar modification

--- printcetnrum_web.client/src/app/navbar/navbar.component.ts
+++ printcetnrum_web.client/src/app/navbar/navbar.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent {
   currentUrl: string | undefined;
+  menuValue: boolean = false;
+  menu_icon: string = 'bi bi-list';
   constructor(
     private router: Router,
     private http: HttpClient
@@ -20,5 +22,15 @@ export class NavbarComponent {
     this.router.events.subscribe(() => {
       this.currentUrl = this.router.url;
     });
+  }
+
+  openMenu() {
+    this.menuValue = !this.menuValue;
+    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+  }
+
+  closeMenu() {
+    this.menuValue = false;
+    this.menu_icon = 'bi bi-list';
   }
 }
