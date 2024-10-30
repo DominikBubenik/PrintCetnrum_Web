96abcb871f52fd2bfb49fa5c208a7cb2eeaf0ee2
Dominik Bubenik <bubenik1@stud.uniza.sk>
2024-10-30 09:59:23 +0100

    adding new features such as validations and UI changes

--- printcetnrum_web.client/src/app/app.module.ts
+++ printcetnrum_web.client/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +27,10 @@ import { RegisterPageComponent } from './register-page/register-page.component';
     RegisterPageComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, MatToolbarModule, MatButtonModule, MatIconModule,
+    BrowserModule,
+    HttpClientModule,
+    MatToolbarModule, MatButtonModule, MatIconModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [
--- printcetnrum_web.client/src/app/login-page/login-page.component.ts
+++ printcetnrum_web.client/src/app/login-page/login-page.component.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+/**TODO make the form look better
+ * add option to log in via google
+ */
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  isPasswordVisible: boolean = false;
+  public loginForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    //private auth: AuthService,
+    //private router: Router,
+    //private toast: NgToastService,
+/*    private userStore: UserStoreService*/
+  ) { }
 
-  submitForm() {
-    
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required],
+    });
+  }
+
+  togglePasswordVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted', this.loginForm.value);
+      //this.auth.signIn(this.loginForm.value).subscribe({
+      //  next: (res) => {
+      //    console.log(res.message);
+      //    this.loginForm.reset();
+      //    this.auth.storeToken(res.accessToken);
+      //    this.auth.storeRefreshToken(res.refreshToken);
+      //    const tokenPayload = this.auth.decodedToken();
+      //    this.userStore.setFullNameForStore(tokenPayload.name);
+      //    this.userStore.setRoleForStore(tokenPayload.role);
+      //    this.toast.success({ detail: "SUCCESS", summary: res.message, duration: 5000 });
+      //    this.router.navigate(['dashboard'])
+      //  },
+      //  error: (err) => {
+      //    this.toast.error({ detail: "ERROR", summary: "Something when wrong!", duration: 5000 });
+      //    console.log(err);
+      //  },
+      //});
+    } else {
+      //ValidateForm.validateAllFormFields(this.loginForm);
+    }
   }
 }
