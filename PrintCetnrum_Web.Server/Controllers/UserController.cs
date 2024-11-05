6c06136d8daf1acfbeccaf4651d732108fdc0f75
Dominik Bubenik <bubenik1@stud.uniza.sk>
2024-11-05 17:04:32 +0100

    creating UserController for communication with database

---
+++ PrintCetnrum_Web.Server/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrintCetnrum_Web.Server.Context;
+using PrintCetnrum_Web.Server.Models;
+
+namespace PrintCetnrum_Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _authContext;
+
+        public UserController(AppDbContext authContext)
+        {
+            _authContext = authContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userParam)
+        {
+            if (userParam == null)
+                return BadRequest();
+
+            var user = await _authContext.Users.SingleOrDefaultAsync(x => x.UserName == userParam.UserName && x.Password == userParam.Password);
+
+            if (user == null)
+                return NotFound(new { message = "User Not Found" });
+
+            return Ok(new { message = "Login success" });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] User userParam)
+        {
+            if (userParam == null)
+                return BadRequest();
+
+            var user = await _authContext.Users.SingleOrDefaultAsync(x => x.UserName == userParam.UserName);
+
+            if (user != null)
+                return BadRequest(new { message = "Username already exists" });
+
+            await _authContext.Users.AddAsync(userParam);
+            await _authContext.SaveChangesAsync();
+
+            return Ok(new { message = "User Created Successfully" });
+        }
+    }
+}
