0840f826137cda62a365fa2aa10c879afb81e9da
Dominik Bubenik <bubenik1@stud.uniza.sk>
2024-11-07 09:48:10 +0100

    adding authorization service

---
+++ printcetnrum_web.client/src/app/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl = 'https://localhost:7074/api/User/';
+  constructor(private http: HttpClient) { }
+
+  loginUser(user: any) {
+    console.log(user.surname);
+    return this.http.post<any>(this.baseUrl + 'authenticate', user);
+  }
+
+
+  registerUser(user: any) {
+    return this.http.post<any>(this.baseUrl + 'register', user);
+  }
+}
