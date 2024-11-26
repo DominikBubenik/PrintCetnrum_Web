1cab77a6612a32292b4c26a3e4bb7b1304c30d07
Dominik Bubenik <bubenik1@stud.uniza.sk>
2024-11-26 17:37:32 +0100

    fix code style auth.guard

--- printcetnrum_web.client/src/app/guards/auth.guard.ts
+++ printcetnrum_web.client/src/app/guards/auth.guard.ts
@@ -1,8 +1,3 @@
-//import { CanActivateFn } from '@angular/router';
-
-//export const authGuard: CanActivateFn = (route, state) => {
-//  return true;
-//};
 import { AuthService } from './../services/auth.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
@@ -14,6 +9,7 @@ export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {
 
   }
+  
   canActivate(): boolean {
     if (this.auth.isLoggedIn()) {
       return true
