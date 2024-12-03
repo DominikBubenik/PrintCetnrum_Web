c8084cb6a66965b23117926f289dde4a17fac500
Dominik Bubenik <bubenik1@stud.uniza.sk>
2024-12-03 16:56:50 +0100

    add delete and update of users

--- printcetnrum_web.client/src/app/services/user-store.service.ts
+++ printcetnrum_web.client/src/app/services/user-store.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,8 @@ import { BehaviorSubject } from 'rxjs';
 export class UserStoreService {
   private fullName$ = new BehaviorSubject<string>("");
   private role$ = new BehaviorSubject<string>("");
-
-  constructor() { }
+  private baseUrl = 'https://localhost:7074/api/User/';
+  constructor(private http: HttpClient) { }
 
   public getRoleFromStore() {
     return this.role$.asObservable();
@@ -24,5 +25,14 @@ export class UserStoreService {
 
   public setFullNameForStore(fullname: string) {
     this.fullName$.next(fullname)
+  }
+
+  public updateUser(id: number, user: any) {
+    return this.http.put<any>(`${this.baseUrl}update/${id}`, user);
+  }
+
+  // Backend: Delete user
+  public deleteUser(id: number) {
+    return this.http.delete<any>(`${this.baseUrl}delete/${id}`);
   }
 }
