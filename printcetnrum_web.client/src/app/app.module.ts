a7f20ca06f55f4b933d36e4892e8ef82f0e31c14
Dominik Bubenik <bubenik1@stud.uniza.sk>
2025-03-24 23:13:51 +0100

    delete unused stuff

--- printcetnrum_web.client/src/app/app.module.ts
+++ printcetnrum_web.client/src/app/app.module.ts
@@ -28,7 +28,6 @@ import { ConfirmModalComponent } from './shared/confirm-modal/confirm-modal.comp
 import { StampPageComponent } from './pages/stamp-page/stamp-page.component'
 import { FileCardComponentComponent } from './shared/file-card-component/file-card-component.component';
 import { DiplomaPageComponent } from './pages/diploma-page/diploma-page.component';
-import { GeneralTextComponent } from './shared/general-text/general-text.component';
 import { UserProfilePageComponent } from './pages/user-profile-page/user-profile-page.component';
 import { UploadFileComponent } from './pages/upload-file/upload-file.component'
 
@@ -51,7 +50,6 @@ import { UploadFileComponent } from './pages/upload-file/upload-file.component'
     StampPageComponent,
     FileCardComponentComponent,
     DiplomaPageComponent,
-    GeneralTextComponent,
     UserProfilePageComponent,
     UploadFileComponent
   ],
