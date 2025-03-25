ea3c526741c807412d3fe90e628444086128ed46
Dominik Bubenik <bubenik1@stud.uniza.sk>
2025-03-25 09:38:25 +0100

    image shows

--- printcetnrum_web.client/src/app/shared/selected-files-list/selected-files-list.component.ts
+++ printcetnrum_web.client/src/app/shared/selected-files-list/selected-files-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { UserFile } from '../../models/user-file';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-selected-files-list',
@@ -7,15 +8,25 @@ import { UserFile } from '../../models/user-file';
   styleUrl: './selected-files-list.component.css'
 })
 export class SelectedFilesListComponent {
-  files: UserFile[] = []; // Load user files here
   @Input() selectedFiles: UserFile[] = [];
+  baseUrl = environment.apiUrl;
 
-  toggleFileSelection(file: UserFile) {
-    const index = this.selectedFiles.findIndex(f => f.id === file.id);
-    if (index > -1) {
-      this.selectedFiles.splice(index, 1); // Deselect file
-    } else {
-      this.selectedFiles.push(file); // Select file
+  getFileThumbnail(file: UserFile): string | null {
+    // Logic to get file thumbnail based on file type
+    if (this.isImage(file.extension)) {
+      return this.baseUrl + file.filePath; // Assuming filePath contains the image URL
     }
+    return null;
+  }
+
+  isImage(extension: string): boolean {
+    const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'];
+    return imageExtensions.includes(extension.toLowerCase());
+  }
+
+  truncateFileName(fileName: string, maxLength: number = 20): string {
+    return fileName.length > maxLength
+      ? fileName.substring(0, maxLength) + '...'
+      : fileName;
   }
 }
